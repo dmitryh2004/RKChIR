d74f8c6856dbd154f6d95ff3ec3e252b22a08588
dmitry_h <113709033+dmitryh2004@users.noreply.github.com>
2023-09-26 12:05:20 +0300

    26.09.23 12:06

--- practics/js/nav_show.js
+++ practics/js/nav_show.js
@@ -1,21 +1,56 @@
-function ShowNavigation()
+function ShowElement(show_btn_id, hide_btn_id, hideable_id)
 {
-    let show_btn = document.getElementById("show_nav");
-    let hide_btn = document.getElementById("hide_nav");
-    let hideable = document.getElementById("nav_hideable");
-
+    let show_btn = document.getElementById(show_btn_id);
+    let hide_btn = document.getElementById(hide_btn_id);
+    let hideable = document.getElementById(hideable_id);
     show_btn.style.display = "none";
     hide_btn.style.display = "inline-block";
     hideable.style.display = "block";
 }
 
-function HideNavigation()
+function HideElement(show_btn_id, hide_btn_id, hideable_id)
 {
-    let show_btn = document.getElementById("show_nav");
-    let hide_btn = document.getElementById("hide_nav");
-    let hideable = document.getElementById("nav_hideable");
-
+    let show_btn = document.getElementById(show_btn_id);
+    let hide_btn = document.getElementById(hide_btn_id);
+    let hideable = document.getElementById(hideable_id);
     show_btn.style.display = "inline-block";
     hide_btn.style.display = "none";
     hideable.style.display = "none";
+}
+
+function ShowNavigation()
+{
+    ShowElement("show_nav", "hide_nav", "nav_hideable");
+}
+
+function HideNavigation()
+{
+    HideElement("show_nav", "hide_nav", "nav_hideable");
+}
+
+function ShowContacts()
+{
+    ShowElement("show_contacts", "hide_contacts", "contacts_hideable");
+}
+
+function HideContacts()
+{
+    HideElement("show_contacts", "hide_contacts", "contacts_hideable");
+}
+
+function ShowMapImage()
+{
+    ShowElement("show_map", "hide_map", "map_hideable");
+}
+
+function HideMapImage()
+{
+    HideElement("show_map", "hide_map", "map_hideable");
+}
+
+function init()
+{
+    ShowNavigation();
+    HideContacts();
+    HideMapImage();
 }
